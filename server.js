839e1352f083e304bc9124f4a84ea0e8d55593c1
Ryan Njoroge <keddelyronjoz@gmail.com>
2020-09-29 14:06:44 +0300

    Moved server js to the outside directory

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../client/build"));
+  app.use(express.static("client/build"));
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
